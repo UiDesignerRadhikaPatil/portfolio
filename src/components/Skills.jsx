bb04c15fc987a9212f43dce4ad298689b7f012b4
Radhika Patil <144344274+UiDesignerRadhikaPatil@users.noreply.github.com>
2024-11-25 17:04:53 +0530

    Your commit message

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,74 +1,73 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useContext } from 'react';
+import { Timeline, TimelineItem } from 'vertical-timeline-component-for-react';
+import { Container } from 'react-bootstrap';
 import ReactMarkdown from 'react-markdown';
 import PropTypes from 'prop-types';
+import { ThemeContext } from 'styled-components';
 import Fade from 'react-reveal';
-import { Container } from 'react-bootstrap';
 import Header from './Header';
 import endpoints from '../constants/endpoints';
 import FallbackSpinner from './FallbackSpinner';
+import '../css/skills.css'; // Ensure this path is correct
 
-const styles = {
-  iconStyle: {
-    height: 75,
-    width: 75,
-    margin: 10,
-    marginBottom: 0,
-  },
-  introTextContainer: {
-    whiteSpace: 'pre-wrap',
-  },
-};
-
-function Skills(props) {
+const Skills = (props) => {
+  const theme = useContext(ThemeContext);
   const { header } = props;
   const [data, setData] = useState(null);
 
-  const renderSkillsIntro = (intro) => (
-    <h4 style={styles.introTextContainer}>
-      <ReactMarkdown children={intro} />
-    </h4>
-  );
-
   useEffect(() => {
-    fetch(endpoints.skills, {
-      method: 'GET',
-    })
+    fetch(endpoints.skills, { method: 'GET' })
       .then((res) => res.json())
       .then((res) => setData(res))
-      .catch((err) => err);
+      .catch((err) => console.error(err));
   }, []);
 
   return (
     <>
       <Header title={header} />
       {data ? (
-        <Fade>
-          <div className="section-content-container">
-            <Container>
-              {renderSkillsIntro(data.intro)}
-              {data.skills?.map((rows) => (
-                <div key={rows.title}>
-                  <br />
-                  <h3>{rows.title}</h3>
-                  {rows.items.map((item) => (
-                    <div key={item.title} style={{ display: 'inline-block' }}>
-                      <img
-                        style={styles.iconStyle}
-                        src={item.icon}
-                        alt={item.title}
-                      />
-                      <p>{item.title}</p>
-                    </div>
-                  ))}
-                </div>
+        <div className="section-content-container">
+          <Container>
+            {/* Display Introduction as Information */}
+            <Fade>
+              <div className="skills-intro">
+                <ReactMarkdown children={data.intro} />
+              </div>
+            </Fade>
+
+            {/* Timeline for Skills */}
+            <Timeline lineColor={theme.timelineLineColor}>
+              {data.skills?.map((category) => (
+                <Fade key={category.title}>
+                  <TimelineItem
+                    dateText={category.title}
+                    dateInnerStyle={{ background: theme.accentColor }}
+                    bodyContainerStyle={{ color: theme.color }}
+                  >
+                    <ul className="skills-list">
+                      {category.items.map((skill) => (
+                        <li key={skill.title} className="skill-item">
+                          <img
+                            src={skill.icon}
+                            alt={skill.title}
+                            className="skill-icon"
+                          />
+                          <span className="skill-name">{skill.title}</span>
+                        </li>
+                      ))}
+                    </ul>
+                  </TimelineItem>
+                </Fade>
               ))}
-            </Container>
-          </div>
-        </Fade>
-      ) : <FallbackSpinner /> }
+            </Timeline>
+          </Container>
+        </div>
+      ) : (
+        <FallbackSpinner />
+      )}
     </>
   );
-}
+};
 
 Skills.propTypes = {
   header: PropTypes.string.isRequired,
